527f5fb57e7cd087dff56401100690bc138764bd
anicegbem <matthewdlama4@gmail.com>
2022-09-08 15:08:46 +0100

    edit play function with count, et al

--- game.js
+++ game.js
@@ -8,34 +8,67 @@ function getComputerChoice() {
 
 function play(playerSelection, computerSelection) {
     // let computer = getComputerChoice();
-
+    let count = 1;
+    let computer = 0;
+    let player = 0;
     if (playerSelection === computerSelection) {
-        return 'Tie game'
+        console.log(computer = 0);
+        console.log(player = 0);
+        return (
+            'Tie game'
+            
+
+        )
     }
 
     if (playerSelection === 'rock' && computerSelection != 'scissors') {
+        console.log(computer += count);
+        console.log(player = 0);
         return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`);
     }
     else if (playerSelection === 'paper' && computerSelection != 'rock') {
+        console.log(computer += count);
+        console.log(player = 0);
         return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`)
 
 
     }
     else if (playerSelection === 'scissors' && computerSelection != 'paper') {
+        console.log(computer += count);
+        console.log(player = 0);
         return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`);
 
 
     }
     else {
+        console.log(player += count);
+        console.log(computer = 0);
         return ("You win! " + `${playerSelection}` + " defeats " + `${computerSelection}`);
     }
   
 
 }
 
-// const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
-console.log(play('rock', computerSelection))
+
+ 
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playerSelection = prompt("Enter either rock, paper or scissors");
+        computerSelection = getComputerChoice();
+        console.log(play(playerSelection, computerSelection));
+        continue;
+        
+        
+        
+    }
+    // return play(playerSelection, computerSelection);
+}
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+
+// console.log(play('rock', computerSelection))
+console.log(game());
 
 // console.log(getComputerChoice())
