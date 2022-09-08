edaf10dcf0a85a3e2ca5ed1217c169b1c348851e
anicegbem <matthewdlama4@gmail.com>
2022-09-08 13:17:11 +0100

    add play function for player

--- game.js
+++ game.js
@@ -6,4 +6,36 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice())
+function play(playerSelection, computerSelection) {
+    // let computer = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        return 'Tie game'
+    }
+
+    if (playerSelection === 'rock' && computerSelection != 'scissors') {
+        return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`);
+    }
+    else if (playerSelection === 'paper' && computerSelection != 'rock') {
+        return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`)
+
+
+    }
+    else if (playerSelection === 'scissors' && computerSelection != 'paper') {
+        return ("You lose! " + `${computerSelection}` + " defeats " + `${playerSelection}`);
+
+
+    }
+    else {
+        return ("You win! " + `${playerSelection}` + " defeats " + `${computerSelection}`);
+    }
+  
+
+}
+
+// const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(play('rock', computerSelection))
+
+// console.log(getComputerChoice())
